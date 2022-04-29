4bf83d78fed5cee7deee223f84f8a823e93b157c
Caleb Kirkland <calebkirkland2000@gmail.com>
2022-04-28 19:13:12 -0500

    more menu things

--- src/main.py
+++ src/main.py
@@ -19,9 +19,9 @@ def welcome_screen():
     return selection
 
 def log_in():
-    print("Logging in...\n")
     username = input("Enter your username: ")
     password = input("Enter your password: ")
+    #TO DO: CONNECT TO THE DATABASE
 
     return
 
@@ -34,8 +34,8 @@ def create_account():
     # TO DO: CHECK IF EMAIL EXISTS
     password = input("Enter your password: ")
     print("What is your shipping address?: ")
-    street = input("\tEnter your house address and street:")
-    city = input("\tEnter your city:")
+    street = input("\tEnter your house address and street: ")
+    city = input("\tEnter your city: ")
     state = input("\tEnter your state: ")
     zip_code = input("\tEnter your zip code: ")
 
@@ -51,20 +51,40 @@ def create_account():
                 zip text)''')
 
     cursor.execute("INSERT INTO Users VALUES ('{}','{}','{}','{}','{}','{}','{}','{}','{}')".format(username, first_name, last_name, email, password, street, city, state, zip_code))
+    print("Account created!")
+
+def print_menu():
+    selection = input("1.) Account Information\n2.) Cart Information\n 3.)Inventory Information\nMake a selection (1, 2, or 3): ")
+    return selection
 
 def main():
-    # Printing the welcome screen
-    user_selection = welcome_screen()
+    # Printing the welcome screen and taking input
+    welcome_selection = welcome_screen()
 
-    if user_selection == '1':
+    if welcome_selection == '1':
         os.system('cls' if os.name == 'nt' else 'clear') # Clearing the screen for the next menu
         log_in()
-    elif user_selection == '2':
+    elif welcome_selection == '2':
         os.system('cls' if os.name == 'nt' else 'clear')
         create_account()
-    elif user_selection == '3':
+    elif welcome_selection == '3':
         exit() # Exit the Program
 
+    # Printing the menu and taking input
+    menu_selection = print_menu()
+
+    if menu_selection == '1':
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Works")
+        # TO DO: ACCOUNT INFO
+    elif menu_selection == '2':
+        os.system('cls' if os.name == 'nt' else 'clear')
+        # TO DO: CART INFO
+    elif menu_selection == '3':
+        os.system('cls' if os.name == 'nt' else 'clear')
+        # TO DO: INVENTORY INFO
+
+        
     # Commiting the changes to the database (be careful!)
     connection.commit()
     # Closing the connection
